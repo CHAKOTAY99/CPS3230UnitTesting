0cc1500f8d8f8e2985d9ba031da8ed859f2425fd
Chakotay <chakotay.incorvaja@gmail.com>
2019-10-25 09:49:44 +0200

    Execrise 4 basic done

--- src/main/java/edu/uom/studentdb/Student.java
+++ src/main/java/edu/uom/studentdb/Student.java
@@ -1,4 +1,13 @@
 package edu.uom.studentdb;
 
 public class Student {
+
+    int id;
+    String name, course;
+
+    public Student(int id, String name, String course){
+        this.id = id;
+        this.name = name;
+        this.course = course;
+    }
 }
--- src/main/java/edu/uom/studentdb/StudentDB.java
+++ src/main/java/edu/uom/studentdb/StudentDB.java
@@ -1,4 +1,46 @@
 package edu.uom.studentdb;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StudentDB {
+    List<Student> database;
+    boolean isDirty = false;
+
+    public StudentDB(){
+        database = new ArrayList<Student>();
+    }
+
+    public  int countStudents(){
+        return database.size();
+    }
+
+    public void addStudent(Student s){
+        if(getStudentById(s.id) == null){
+            database.add(s);
+            setDirty(true);
+        }
+    }
+
+    public void removeStudent(int id){
+        database.remove(getStudentById(id));
+        setDirty(true);
+    }
+
+    public Student getStudentById(int id){
+        for(Student s : database){
+            if(s.id == id){
+                return s;
+            }
+        }
+        return null;
+    }
+
+    public boolean isDirty(){
+        return isDirty;
+    }
+
+    public void setDirty(boolean dirty){
+        this.isDirty = dirty;
+    }
 }

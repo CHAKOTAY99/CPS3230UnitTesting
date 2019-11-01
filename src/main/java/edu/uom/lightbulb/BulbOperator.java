52b1383a15f6666c485154379c9672cabed37af2
Chakotay <chakotay.incorvaja@gmail.com>
2019-11-01 08:09:16 +0100

    Added more code to finish the bulb

--- src/main/java/edu/uom/lightbulb/BulbOperator.java
+++ src/main/java/edu/uom/lightbulb/BulbOperator.java
@@ -1,4 +1,131 @@
 package edu.uom.lightbulb;
 
 public class BulbOperator {
+		private boolean lowBrightness = false, mediumBrightness = false, highBrightness = false;
+
+	boolean turnOn() {
+		if (!(lowBrightness || mediumBrightness || highBrightness)) {
+			mediumBrightness = true;
+			return true;
+		} else {
+			throw new IllegalStateException();
+		}
+	}
+
+	boolean turnOff() {
+		if (lowBrightness || mediumBrightness || highBrightness) {
+			lowBrightness = false;
+			mediumBrightness = false;
+			highBrightness = false;
+			return true;
+		} else {
+			throw new IllegalStateException();
+		}
+	}
+
+	boolean increaseBrightness() {
+		if (lowBrightness) {
+			lowBrightness = false;
+			mediumBrightness = true;
+		} else if (mediumBrightness) {
+			mediumBrightness = false;
+			highBrightness = true;
+		} else {
+			throw new IllegalStateException();
+		}
+		return true;
+	}
+
+	boolean decreaseBrightness() {
+		if (highBrightness) {
+			highBrightness = false;
+			mediumBrightness = true;
+		} else if (mediumBrightness) {
+			mediumBrightness = false;
+			lowBrightness = true;
+		} else {
+			throw new IllegalStateException();
+		}
+		return true;
+	}
+
+	boolean isLowBrightness() {
+		return lowBrightness;
+	}
+
+	boolean isMediumBrightness() {
+		return mediumBrightness;
+	}
+
+	boolean isHighBrightness() {
+		return highBrightness;
+	}
+
+	/**
+	 *
+	 * @return false if zero or one variable is true. true otherwise
+	 */
+	boolean onlyOneStateTrue() {
+		if (lowBrightness) {
+			return mediumBrightness || highBrightness;
+		} else {
+			return mediumBrightness && highBrightness;
+		}
+	}
+
+	boolean isTurnedOn() {
+		return lowBrightness || mediumBrightness || highBrightness;
+	}
+
+    /*public static void main(String[] args) {
+	final Scanner sc = new Scanner(System.in);
+        final BulbOperator bulbOperator = new BulbOperator();
+
+        System.out.println("---BULB MANAGER---");
+        int choice;
+        do{
+            System.out.println("Chose an option to control your bulb.");
+            System.out.println("1. Turn on bulb.");
+            System.out.println("2. Turn off bulb.");
+            System.out.println("3. Increase brightness.");
+            System.out.println("4. Decrease brightness.");
+            System.out.println("5. Exit.");
+            choice = sc.nextInt();
+            switch(choice){
+                case 1:
+                    if(bulbOperator.turnOn()){
+                        System.out.println("Bulb turned on.");
+                    } else {
+                        System.out.println("Cannot turn on bulb now.");
+                    }
+                    break;
+                case 2:
+                    if(bulbOperator.turnOff()){
+                        System.out.println("Bulb turned off.");
+                    } else {
+                        System.out.println("Bulb can't be turned off now.");
+                    }
+                    break;
+                case 3:
+                    if(bulbOperator.increaseBrightness()){
+                        System.out.println("Bulb brightness increased.");
+                    } else {
+                        System.out.println("Can't increase bulb brightness.");
+                    }
+                    break;
+                case 4:
+                    if(bulbOperator.decreaseBrightness()){
+                        System.out.println("Bulb brightness decreased.");
+                    } else {
+                        System.out.println("Can't decrease bulb brightness.");
+                    }
+                    break;
+                case 5:
+                    System.out.println("Bye.");
+                    break;
+                default:
+                    System.out.println("Incorrect option chosen.");
+            }
+        } while(choice != 5);
+    }*/
 }
--- src/test/java/edu/uom/lightbulb/BulbOperatorModelTest.java
+++ src/test/java/edu/uom/lightbulb/BulbOperatorModelTest.java
@@ -3,24 +3,31 @@ package edu.uom.lightbulb;
 import edu.uom.lightbulb.enums.BulbOperatorStates;
 import nz.ac.waikato.modeljunit.Action;
 import nz.ac.waikato.modeljunit.FsmModel;
+import nz.ac.waikato.modeljunit.GreedyTester;
+import nz.ac.waikato.modeljunit.StopOnFailureListener;
+import nz.ac.waikato.modeljunit.coverage.ActionCoverage;
+import nz.ac.waikato.modeljunit.coverage.StateCoverage;
+import nz.ac.waikato.modeljunit.coverage.TransitionPairCoverage;
+import org.junit.Test;
+
+import java.util.Random;
 
 import static org.junit.Assert.assertEquals;
 
 public class BulbOperatorModelTest implements FsmModel {
+    //Linking the SUT
+    private BulbOperator systemUnderTest = new BulbOperator();
 
-    // Link the system under test
-    private  BulbOperator systemUnderTest = new BulbOperator();
-
-    // State Variables
+    //State Variables
     private BulbOperatorStates modelBulb = BulbOperatorStates.OFF;
     private boolean lowBrightness = false, mediumBrightness = false, highBrightness = false;
 
-    // Method implementations
-    public BulbOperatorStates getState(){
+    //Method implementations
+    public BulbOperatorStates getState() {
         return modelBulb;
     }
 
-    public void reset (final boolean var1) {
+    public void reset(final boolean var1) {
         if (var1) {
             systemUnderTest = new BulbOperator();
         }
@@ -30,73 +37,106 @@ public class BulbOperatorModelTest implements FsmModel {
         highBrightness = false;
     }
 
-    // Transactions including guards
-    public boolean turnOnBulbGuard(){
+    //Transitions incl. guards
+    public boolean turnOnBulbGuard() {
         return getState().equals(BulbOperatorStates.OFF);
     }
-
-    public @Action void turnOnBulb(){
-        // Updating System Under Test
+    public @Action void turnOnBulb() {
+        //Updating SUT
         systemUnderTest.turnOn();
 
-        // Updating Model
+        //Updating model
         modelBulb = BulbOperatorStates.MEDIUM_BRIGHTNESS;
         mediumBrightness = true;
 
-        // Checking the correspondence between the model and the system under test
-        assertEquals("The SUT's bulb does not match the model's bulb after turing on the bulb", mediumBrightness, systemUnderTest.isMediumBrightness());
-
-        // Additional safety measure test - was not required
+        //Checking correspondence between model and SUT.
+        assertEquals("The SUT's bulb does not match the model's bulb after turning on the bulb", mediumBrightness, systemUnderTest.isMediumBrightness());
         assertEquals("SUT has multiple brightness values at once.", lowBrightness || highBrightness, systemUnderTest.onlyOneStateTrue());
     }
 
-    public boolean turnOffBulbGuard(){
+    public boolean turnOffBulbGuard() {
         return !getState().equals(BulbOperatorStates.OFF);
     }
-
     public @Action void turnOffBulb() {
-        // Updating System Under Test
+        //Updating SUT
         systemUnderTest.turnOff();
 
-        // Updating Model
+        //Updating model
         modelBulb = BulbOperatorStates.OFF;
         lowBrightness = false;
         highBrightness = false;
         mediumBrightness = false;
 
-        // Checking the correspondence between the model and the system under test
-        assertEquals("The SUT's bulb does not match the model's bulb after tuning off the bulb", lowBrightness || mediumBrightness || highBrightness,
-                systemUnderTest.isTurnedOn());
+        //Checking correspondence between model and SUT.
+        assertEquals("The SUT's bulb does not match the model's bulb after turning off the bulb", lowBrightness || mediumBrightness || highBrightness, systemUnderTest.isTurnedOn());
     }
 
-    public boolean decreaseBulbBrightnessGuard(){
+    public boolean decreaseBulbBrightnessGuard() {
         return getState().equals(BulbOperatorStates.HIGH_BRIGHTNESS) || getState().equals(BulbOperatorStates.MEDIUM_BRIGHTNESS);
     }
-
     public @Action void decreaseBulbBrightness() {
-        // Updating SUT
+        //Updating SUT
         systemUnderTest.decreaseBrightness();
 
-        //Checking correspondence between model and SUT.
-        assertEquals("The SUT's bulb does not match the model's bulb after decreasing the bulb's brightness", mediumBrightness, systemUnderTest.isMediumBrightness());
-        assertEquals("SUT has multiple brightness values at once.", lowBrightness || highBrightness, systemUnderTest.onlyOneStateTrue());
-    } else
-    {
-        modelBulb = BulbOperatorStates.LOW_BRIGHTNESS;
-        mediumBrightness = false;
-        lowBrightness = true;
+        //Updating model
+        if (getState().equals(BulbOperatorStates.HIGH_BRIGHTNESS)) {
+            modelBulb = BulbOperatorStates.MEDIUM_BRIGHTNESS;
+            highBrightness = false;
+            mediumBrightness = true;
 
-        //Checking correspondence between model and SUT.
-        assertEquals("The SUT's bulb does not match the model's bulb after decreasing the bulb's brightness", lowBrightness, systemUnderTest.isLowBrightness());
-        assertEquals("SUT has multiple brightness values at once.", mediumBrightness || highBrightness, systemUnderTest.onlyOneStateTrue());
+            //Checking correspondence between model and SUT.
+            assertEquals("The SUT's bulb does not match the model's bulb after decreasing the bulb's brightness", mediumBrightness, systemUnderTest.isMediumBrightness());
+            assertEquals("SUT has multiple brightness values at once.", lowBrightness || highBrightness, systemUnderTest.onlyOneStateTrue());
+        } else {
+            modelBulb = BulbOperatorStates.LOW_BRIGHTNESS;
+            mediumBrightness = false;
+            lowBrightness = true;
+
+            //Checking correspondence between model and SUT.
+            assertEquals("The SUT's bulb does not match the model's bulb after decreasing the bulb's brightness", lowBrightness, systemUnderTest.isLowBrightness());
+            assertEquals("SUT has multiple brightness values at once.", mediumBrightness || highBrightness, systemUnderTest.onlyOneStateTrue());
+        }
     }
 
     public boolean increaseBulbBrightnessGuard() {
         return getState().equals(BulbOperatorStates.LOW_BRIGHTNESS) || getState().equals(BulbOperatorStates.MEDIUM_BRIGHTNESS);
     }
-
-    public @Action void increaseBulbBrightness(){
-        // Updating SUT
+    public @Action void increaseBulbBrightness() {
+        //Updating SUT
         systemUnderTest.increaseBrightness();
+
+        //Updating model
+        if (getState().equals(BulbOperatorStates.LOW_BRIGHTNESS)) {
+            modelBulb = BulbOperatorStates.MEDIUM_BRIGHTNESS;
+            lowBrightness = false;
+            mediumBrightness = true;
+
+            //Checking correspondence between model and SUT.
+            assertEquals("The SUT's bulb does not match the model's bulb after increasing the bulb's brightness", mediumBrightness, systemUnderTest.isMediumBrightness());
+            assertEquals("SUT has multiple brightness values at once.", lowBrightness || highBrightness, systemUnderTest.onlyOneStateTrue());
+        } else {
+            modelBulb = BulbOperatorStates.HIGH_BRIGHTNESS;
+            mediumBrightness = false;
+            highBrightness = true;
+
+            //Checking correspondence between model and SUT.
+            assertEquals("The SUT's bulb does not match the model's bulb after increasing the bulb's brightness", highBrightness, systemUnderTest.isHighBrightness());
+            assertEquals("SUT has multiple brightness values at once.", lowBrightness || mediumBrightness, systemUnderTest.onlyOneStateTrue());
+        }
+    }
+
+    //Test runner
+    @Test
+    public void BulbOperatorModelRunner() {
+        final GreedyTester tester = new GreedyTester(new BulbOperatorModelTest()); //Creates a test generator that can generate random walks. A greedy random walk gives preference to transitions that have never been taken before. Once all transitions out of a state have been taken, it behaves the same as a random walk.
+        tester.setRandom(new Random()); //Allows for a random path each time the model is run.
+        tester.buildGraph(); //Builds a model of our FSM to ensure that the coverage metrics are correct.
+        tester.addListener(new StopOnFailureListener()); //This listener forces the test class to stop running as soon as a failure is encountered in the model.
+        tester.addListener("verbose"); //This gives you printed statements of the transitions being performed along with the source and destination states.
+        tester.addCoverageMetric(new TransitionPairCoverage()); //Records the transition pair coverage i.e. the number of paired transitions traversed during the execution of the test.
+        tester.addCoverageMetric(new StateCoverage()); //Records the state coverage i.e. the number of states which have been visited during the execution of the test.
+        tester.addCoverageMetric(new ActionCoverage()); //Records the number of @Action methods which have ben executed during the execution of the test.
+        tester.generate(500); //Generates 500 transitions
+        tester.printCoverage(); //Prints the coverage metrics specified above.
     }
 }
